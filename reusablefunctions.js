50debbf39139ab68e6a45071b78dd05707a2807a
Shayla Brown <shaylabrown227@gmail.com>
2024-04-20 20:47:00 -0500

    took an array of numbers and returned the average

--- reusablefunctions.js
+++ reusablefunctions.js
@@ -1,9 +1,17 @@
 console.log("Creating Reusable Functions!!")
 
-function add ([a, b]) {
-    let sum = a + b
-    return a + b
-  }
-  
-  const sum = add(5, 10)
-  console.log(sum)
+//Calculate sum
+function add(a, b, c) {
+    let sum = a + b + c
+    return a + b + c
+}
+
+const sum = add(5, 10, 15)
+console.log(sum)
+
+//calculate AVERAGE
+var a = [3 + 5 + 7];
+
+a.forEach(function (calculateAverage) {
+    console.log(calculateAverage);
+});
